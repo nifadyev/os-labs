1100b3b61acd7703f33fe8fab58be03c4a533aa8
vadim nifadyev <vnifadyev@gmail.com>
2018-03-22 20:31:46 +0300

    Exec checks have been added

--- lab4(Pipes in UNix)/lab4.c
+++ lab4(Pipes in UNix)/lab4.c
@@ -1,87 +1,88 @@
+#include <unistd.h>
 #include <sys/types.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
-
+ 
 int main()
 {
-	int fd1[2], fd2[2]; // in/out chanels for pipes
-	pid_t childPID1, childPID2;
-	int pipe1, pipe2;
+        int pipe1,pipe2;
+        int fd1[2],fd2[2];
+        pid_t child,grandchild;
+ 
+        pipe1=pipe(fd1);
+        child=fork();
+        if(pipe1<0)
+        {
+            printf("Failed to create pipe\n");
+            return EXIT_FAILURE;
+        }
+        if(child<0)
+        {
+            printf("Failed to fork\n");
+            return EXIT_FAILURE;
+        }
 
-	pipe1 = pipe(fd1);
-	childPID1 = fork();
+        else if(child == 0) // Child process
+        {
+            pipe2=pipe(fd2);
+            grandchild=fork();
 
-	if(pipe1 < 0)
-	{
-		printf("Error! Cannot create pipe\n");
-		return EXIT_FAILURE;
-	}
-	if(childPID1 < 0)
-	{
-		printf("Error! Cannot create child process\n");
-		return EXIT_FAILURE;
-	}
+            if(pipe2<0)
+            {
+                printf("Failed to create pipe\n");
+                return EXIT_FAILURE;
+            }
+            if(grandchild < 0)
+            {
+                printf("Failed to fork\n");
+                return EXIT_FAILURE;
+            }
 
-	else if(childPID1 == 0) // Child process
-	{
-		pipe(fd2);
-		childPID2 = fork();
+            else if (grandchild == 0) // Grandchild process
+            {
+                close (1);
+                dup2 (fd2[1],1);
+                close (fd2[0]);
+                close (fd2[1]);
 
-		if(pipe2 < 0)
-		{
-			printf("Error! Cannot create pipe\n");
-			return EXIT_FAILURE;
-		}
-		if (childPID2 < 0)
-		{
-			printf("Error! Cannot create grandchild\n");
-			return EXIT_FAILURE;
-		}
+                if(execl("/bin/ls", "ls", "-la", "/dev", NULL) == -1)
+                {
+                    printf("Exec error\n");
+                    return EXIT_FAILURE;
+                }
+            }
+            else // Child process
+            {
+                close (0);
+                dup2 (fd2[0],0);
+                close (fd2[0]);
+                close (fd2[1]);
 
-		else if (childPID2 == 0) // Grandchild process
-		{
-      // Closing stdin
-      close(0);
-      dup2(fd1[0], 0);
-      close(fd1[0]);
-      close(fd1[1]);
+                close (1);
+                dup2(fd1[1],1);
+                close (fd1[0]);
+                close (fd1[1]);
 
-      // Closing stdiout
-      close(1);
-      dup2(fd2[1], 1);
-      close(fd2[0]);
-      close(fd2[1]);
+                if(execlp ("sort", "sort", NULL) == -1)
+                {
+                    printf("Exec error\n");
+                    return EXIT_FAILURE;
+                }
+            }
+        }
+        else
+        {
+            close (0);
+            dup2 (fd1[0],0);
+            close (fd1[0]);
+            close (fd1[1]);
 
-			execl("/bin/ls", "ls", "-la", "/dev", NULL);
-		}
-		else // Child process
-		{
-      // Closing stdin
-			close(0);
-      dup2(fd2[0], 0);
-      close(fd2[0]);
-      close(fd2[1]);
-
-      // Closing stdiout
-			close(1);
-			dup2(fd1[1], 1);
-			close(fd1[0]);
-			close(fd1[1]);
-
-			execlp("sort", "sort", NULL);
-		}
-	}
-	else // Parent
-	{
-    // Closing stdin
-		close(0);
-		dup2(fd1[0], 0);
-		close(fd1[0]);
-		close(fd1[1]);
-    
-		execlp("more", "more", NULL);
-	}
-
-	return 0;
+            if(execlp ("more", "more", NULL) == -1)
+            {
+                printf("Exec Error\n");
+                return EXIT_FAILURE;
+            }
+        }
+ 
+    return 0;
 }
