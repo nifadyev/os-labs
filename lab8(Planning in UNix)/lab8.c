89aa348b10cfd7a9e5d5611ec72359cf09593ce4
vadim <vnifadyev@gmail.com>
2018-04-19 20:25:40 +0300

    Raw draft of FCFS scheduling

--- lab8(Planning in UNix)/lab8.c
+++ lab8(Planning in UNix)/lab8.c
@@ -5,60 +5,13 @@ typedef struct
 {
     int stringLength;
     char *data;
-    int currentPosition;
+    int currentPosition; // current status
     int isFinished;
+    int isActive;
 } Thread;
 
-// String proccessing or time interval proccessing
-int f(Thread *threads, int timeInterval, int threadNumber)
-{
-    int i, finishedThreads = 0;
-    int b = 0;        // flag, which tells whether we visited this row/column or not
-    int isActive = 0; // does thread still running?
-    printf("\n%d", timeInterval + 1);
-
-    for (i = 0; i < threadNumber; i++)
-    {
-        if (threads[i].currentPosition >= threads[i].stringLength)
-        {
-            printf("\tx");
-            threads[i].isFinished = 1;
-            finishedThreads++;
-
-            continue;
-        }
-
-        if (threads[i].data[threads[i].currentPosition] == '0' && b == 0)
-        {
-            //printf("\t0HERE");
-            printf("\t0");
-            isActive = 1;
-            b = 1;
-            threads[i].currentPosition++;
-        }
-
-        else if (threads[i].data[threads[i].currentPosition] == '0' /*  && b != 0 */)
-        {
-            //printf("\t.THERE");
-            printf("\t.");
-        }
-
-        else if (threads[i].data[threads[i].currentPosition] == '-')
-        {
-            isActive = 0;
-            printf("\t|");
-            threads[i].currentPosition++;
-        }
-        //finishedThreads += threads[i].isFinished;
-    }
-
-    if (finishedThreads == threadNumber)
-    {
-        return 0;
-    }
-
-    return 1;
-}
+int PriorityScheduling(Thread *threads, int timeInterval, int threadNumber);
+int FCFSScheduling(Thread *threads, int timeInterval, int threadNumber);
 
 int main()
 {
@@ -68,6 +21,7 @@ int main()
     char **strings;
     Thread *threads;
     FILE *file;
+
     if ((file = fopen(inputFile, "r")) == NULL)
     {
         fclose(file);
@@ -87,10 +41,13 @@ int main()
         strings[i] = (char *)malloc(/* threadNumber */ 50 * sizeof(char));
     }
 
+    printf("Source file\n");
     for (i = 0; i < threadNumber * 2; i++)
     {
         fscanf(file, "%s\n", strings[i]);
+        printf("%s\n", strings[i]);
     }
+    printf("\n");
     threads = (Thread *)malloc(threadNumber * sizeof(Thread));
 
     for (i = 0; i < threadNumber; i++)
@@ -99,13 +56,156 @@ int main()
         threads[i].data = strings[i * 2 + 1];
         threads[i].currentPosition = 0;
         threads[i].isFinished = 0;
+        threads[i].isActive = 0;
     }
 
-    for (i = 0; f(threads, i, threadNumber) /*  && i < 20 */; i++)
+    // printf("Priority scheduling\n");
+    // for (i = 0; PriorityScheduling(threads, i, threadNumber) /*  && i < 20 */; i++)
+    //     // {
+    //     //     printf("\n%d iteration\n", i);
+    //     // }
+    //     ;
+
+    printf("\n\nFCFS scheduling\n");
+    for (i = 0; FCFSScheduling(threads, i, threadNumber) /*  && i < 20 */; i++)
         // {
         //     printf("\n%d iteration\n", i);
         // }
         ;
 
     return 0;
+}
+
+// String proccessing or time interval proccessing
+// From high to low priority
+int PriorityScheduling(Thread *threads, int timeInterval, int threadNumber)
+{
+    int i, finishedThreads = 0;
+    int b = 0; // flag, which tells whether we visited this row/column or not
+    //int isActive = 0; // does thread still running?
+    printf("\n%d", timeInterval + 1);
+
+    for (i = 0; i < threadNumber; i++)
+    {
+        if (threads[i].currentPosition >= threads[i].stringLength)
+        {
+            printf("\tx");
+            threads[i].isFinished = 1;
+            finishedThreads++;
+
+            continue;
+        }
+
+        if (threads[i].data[threads[i].currentPosition] == '0' && b == 0)
+        {
+            // printf("\t0HERE");
+            printf("\t0");
+            //isActive = 1;
+            b = 1;
+            threads[i].currentPosition++;
+        }
+
+        else if (threads[i].data[threads[i].currentPosition] == '0' /*  && b != 0 */)
+        {
+            // printf("\t.THERE");
+            printf("\t.");
+        }
+
+        else if (threads[i].data[threads[i].currentPosition] == '-')
+        {
+            //isActive = 0;
+            printf("\t|");
+            threads[i].currentPosition++;
+        }
+        //finishedThreads += threads[i].isFinished;
+    }
+
+    if (finishedThreads == threadNumber)
+    {
+        return 0;
+    }
+
+    return 1;
+}
+
+int FCFSScheduling(Thread *threads, int timeInterval, int threadNumber)
+{
+    int i, finishedThreads = 0;
+    int b = 0; // flag, which tells whether we visited this row/column or not
+    //int isActive = 0; // does thread still running?
+    printf("\n%d", timeInterval + 1);
+
+    for (i = 0; i < threadNumber; i++)
+    {
+        if (threads[i].currentPosition >= threads[i].stringLength)
+        {
+            printf("\tx");
+            threads[i].isFinished = 1;
+            finishedThreads++;
+
+            continue;
+        }
+
+        // if (threads[i].data[threads[i].currentPosition] == '0')
+        // {
+        //     if (b == 0)
+        //     {
+        //         printf("\t0");
+        //         threads[i].isActive = 1;
+        //         b = 1;
+        //         threads[i].currentPosition++;
+        //     }
+        //     else if (threads[i].isActive == 1)
+        //     {
+        //         threads[i].currentPosition++;
+        //         printf("\t0");
+        //     }
+        //     else if (threads[i].isActive == 0)
+        //     {
+        //         printf("\t.");
+        //     }
+        // }
+
+        // TODO: 3 thread works well but first two didn't come in "ready to run"
+        if (threads[i].data[threads[i].currentPosition] == '0' && b == 0)
+        {
+            // printf("\t0HERE");
+            printf("\t0");
+            threads[i].isActive = 1;
+            b = 1;
+            threads[i].currentPosition++;
+        }
+
+        else if (threads[i].data[threads[i].currentPosition] == '0' && threads[i].isActive == 1)
+        {
+            // printf("\t.THERE");
+            // threads[0].isActive = 0;
+            // threads[1].isActive = 0;
+            // threads[2].isActive = 0;
+            threads[i].currentPosition++;
+            printf("\t0");
+            // threads[i].isActive = 1;
+        }
+
+        else if (threads[i].data[threads[i].currentPosition] == '0' && threads[i].isActive == 0 /*  && b != 0 */)
+        {
+            // printf("\t.THERE");
+            printf("\t.");
+        }
+
+        else if (threads[i].data[threads[i].currentPosition] == '-')
+        {
+            threads[i].isActive = 0;
+            printf("\t|");
+            threads[i].currentPosition++;
+        }
+        //finishedThreads += threads[i].isFinished;
+    }
+
+    if (finishedThreads == threadNumber)
+    {
+        return 0;
+    }
+
+    return 1;
 }
